2af550d2b9004baf30b1418d1d94645559958b9d
Vinaykrishna <72072980+vinaykrishnavvinod@users.noreply.github.com>
2022-09-14 12:12:10 +0530

    inital commit

---
+++ frontend/src/Orderlist.js
@@ -0,0 +1,171 @@
+import truck from "./truck.png"
+import tf from "./tf.png"
+import bb from "./Images/bb.png"
+import tfs from "./tfs.png"
+
+import './Orderlist.css';
+
+function Orderlist() {
+  return (
+    <div className="Orderlist_outercover">
+         
+      <div className='outerlist-l1'>
+      
+        <label> What Shopex Offers!</label>
+      </div>
+
+
+      <div className='outerlist-img-cover'>
+        
+
+        
+  
+      
+
+      
+   
+
+
+<div className="outerlist-hghhfgfgfhgf">
+  
+<div className="orderlisttttt">
+
+<div className='ohyfffkgfkcgt-img-truck'>
+        
+        <img src={truck}/>
+  
+        </div>
+  
+  <div className="oc_img-lbl">
+  <label className="orderlist-img-hndgg">24/7 Support</label>
+  <div className="orderlist-under-lbl-img">
+  <label className="orderlist-under-lbl-img111">Lorem ipsum dolor sit amet</label>
+  <label className="orderlist-under-lbl-img111">consectetur adipiscing elit</label>
+  <label className="orderlist-under-lbl-img111">massa purus gravida</label>
+  </div>
+  </div>
+
+  </div>
+  </div>
+
+<div className="outerlist-hghhfgfgfhgf">
+
+  <div className="orderlisttttt">
+
+<div className='ohyfffkgfkcgt-img-truck'>
+        
+        <img src={tf}/>
+  
+        </div>
+  
+  <div className="oc_img-lbl">
+  <label className="orderlist-img-hndgg">24/7 Support</label>
+  <div className="orderlist-under-lbl-img">
+  <label className="orderlist-under-lbl-img111">Lorem ipsum dolor sit amet</label>
+  <label className="orderlist-under-lbl-img111">consectetur adipiscing elit</label>
+  <label className="orderlist-under-lbl-img111">massa purus gravida</label>
+  </div>
+  </div>
+
+  </div>
+  </div>
+
+
+  <div className="outerlist-hghhfgfgfhgf">
+
+  <div className="orderlisttttt">
+
+<div className='ohyfffkgfkcgt-img-truck'>
+        
+        <img src={bb}/>
+  
+        </div>
+  
+  <div className="oc_img-lbl">
+  <label className="orderlist-img-hndgg">24/7 Support</label>
+  <div className="orderlist-under-lbl-img">
+  <label className="orderlist-under-lbl-img111">Lorem ipsum dolor sit amet</label>
+  <label className="orderlist-under-lbl-img111">consectetur adipiscing elit</label>
+  <label className="orderlist-under-lbl-img111">massa purus gravida</label>
+  </div>
+  </div>
+
+  </div>
+  </div>
+
+
+
+<div className="outerlist-hghhfgfgfhgf">
+  <div className="orderlisttttt">
+
+<div className='ohyfffkgfkcgt-img-truck'>
+        
+        <img src={tfs}/>
+  
+        </div>
+  
+  <div className="oc_img-lbl">
+  <label className="orderlist-img-hndgg">24/7 Support</label>
+  <div className="orderlist-under-lbl-img">
+  <label className="orderlist-under-lbl-img111">Lorem ipsum dolor sit amet</label>
+  <label className="orderlist-under-lbl-img111">consectetur adipiscing elit</label>
+  <label className="orderlist-under-lbl-img111">massa purus gravida</label>
+  </div>
+  </div>
+
+  </div>
+
+  </div>
+
+
+
+  
+
+
+  
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Orderlist;
---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Orderlist from './Orderlist';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Orderlist />
+  </React.StrictMode>
+);
+
