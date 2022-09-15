5fea53650a7b55691a406f40f979b6dbb62bd2d5
Vinaykrishna <72072980+vinaykrishnavvinod@users.noreply.github.com>
2022-09-15 16:46:32 +0530

    new
    new

--- frontend/src/Gv.js
+++ frontend/src/Gv.js
@@ -1,50 +1,299 @@
+import cha from "./Images/cha.png";
+import clr from "./Images/clr.png";
+import soff from "./Images/soff.png";
+import kas from "./Images/kas.png";
+import beg from "./Images/beg.png";
+import watc from "./Images/watc.png";
+import anlg from "./Images/anlg.png";
 
+import rhed from "./Images/rhed.png";
+import pilo from "./Images/pilo.png";
+import rw from "./Images/rw.png";
+import camm from "./Images/camm.png";
+import vh from "./Images/vh.png";
 
-import cha from './Images/cha.png';
-import clr from './Images/clr.png';
-
-
-
-import './style/Gv.css';
+import "./style/Gv.css";
 
 function Gv() {
   return (
+
+    <div className="gv-outer-row-2-main">
     <div className="gv-outer-cover">
-    
-<div className='gv-r1-cover'>
+      <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={cha} />
+          </div>
 
-<div className='gv-r1-chair-1'>
-  <div className='gv-r1-chr-1-pic'>
-    
-    <img src={cha}/>
-     </div>
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
 
-
-
-  
-  <label className='gv-r1-vel-ethit-eusmod'>Vel elit euismod</label>
-<div className='gv-r1-mooncolor'>
-<img src={clr}/>
-</div>
-
-  
-  
-  <div className='gv-chr1-dollor-lbl'> 
-  <label className='gv-r1-blue-dlor'>$26.00 </label>
-  <label className='gv-r1-pink-dollr'>$42.00 </label>
-  </div>
-
-
-</div>
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
 
 
 
-</div>
+      <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={soff} />
+          </div>
+
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
+
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={kas} />
+          </div>
+
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
+
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={beg} />
+          </div>
+
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
+
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
 
 
 
       
     </div>
+
+
+<div className="gv-row-2">
+
+    <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={watc} />
+          </div>
+
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
+
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={anlg} />
+          </div>
+
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
+
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={rhed} />
+          </div>
+
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
+
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="gv-r1-cover">
+        <div className="gv-r1-chair-1">
+          <div className="gv-r1-chr-1-pic">
+            <img src={pilo} />
+          </div>
+
+          <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+          <div className="gv-r1-mooncolor">
+            <img src={clr} />
+          </div>
+
+          <div className="gv-chr1-dollor-lbl">
+            <label className="gv-r1-blue-dlor">$26.00 </label>
+            <label className="gv-r1-pink-dollr">$42.00 </label>
+          </div>
+        </div>
+      </div>
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+      <div className="gv-row-2">
+
+<div className="gv-r1-cover">
+    <div className="gv-r1-chair-1">
+      <div className="gv-r1-chr-1-pic">
+        <img src={rw} />
+      </div>
+
+      <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+      <div className="gv-r1-mooncolor">
+        <img src={clr} />
+      </div>
+
+      <div className="gv-chr1-dollor-lbl">
+        <label className="gv-r1-blue-dlor">$26.00 </label>
+        <label className="gv-r1-pink-dollr">$42.00 </label>
+      </div>
+    </div>
+  </div>
+
+
+  <div className="gv-r1-cover">
+    <div className="gv-r1-chair-1">
+      <div className="gv-r1-chr-1-pic">
+        <img src={camm} />
+      </div>
+
+      <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+      <div className="gv-r1-mooncolor">
+        <img src={clr} />
+      </div>
+
+      <div className="gv-chr1-dollor-lbl">
+        <label className="gv-r1-blue-dlor">$26.00 </label>
+        <label className="gv-r1-pink-dollr">$42.00 </label>
+      </div>
+    </div>
+  </div>
+
+
+  <div className="gv-r1-cover">
+    <div className="gv-r1-chair-1">
+      <div className="gv-r1-chr-1-pic">
+        <img src={vh} />
+      </div>
+
+      <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+      <div className="gv-r1-mooncolor">
+        <img src={clr} />
+      </div>
+
+      <div className="gv-chr1-dollor-lbl">
+        <label className="gv-r1-blue-dlor">$26.00 </label>
+        <label className="gv-r1-pink-dollr">$42.00 </label>
+      </div>
+    </div>
+  </div>
+
+
+  <div className="gv-r1-cover">
+    <div className="gv-r1-chair-1">
+      <div className="gv-r1-chr-1-pic">
+        <img src={beg} />
+      </div>
+
+      <label className="gv-r1-vel-ethit-eusmod">Vel elit euismod</label>
+      <div className="gv-r1-mooncolor">
+        <img src={clr} />
+      </div>
+
+      <div className="gv-chr1-dollor-lbl">
+        <label className="gv-r1-blue-dlor">$26.00 </label>
+        <label className="gv-r1-pink-dollr">$42.00 </label>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+
+
+
+
+
+
+
+  <div className="gv-row-2">
+
+
+  
+ 
+
+  </div>
+
+
+
+
+
+
+
+
+    </div>
+
   );
 }
 
