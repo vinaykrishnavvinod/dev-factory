e4c6fdf4f977c4f5beb2f019d7506a78bb77b93f
Vinaykrishna <72072980+vinaykrishnavvinod@users.noreply.github.com>
2022-09-21 00:58:58 +0530

    nrww

---
+++ frontend/src/Appss.js
@@ -0,0 +1,176 @@
+// import Appss from './Appss';
+import './Appss.css';
+import { MdOutlineMoreVert } from "react-icons/md";
+
+function Appss() {
+  return (
+    <div className="Appss_r1">
+      
+
+
+      <div className='Adminproductfeature-r1-screen'>
+        <div className='Adminproductfeature-r1-screen-lbl'>
+        <label>Screen</label>
+<div className='adminproductfeaturedottt'>
+<MdOutlineMoreVert style={{ color: "grey" }} />
+</div>
+        
+        </div>
+        
+      </div>
+
+
+      <div className='Adminproductfeature-r2-4lbl-cover'>
+        
+        
+
+        <div className='Adminproductfeature-r2-col-1'>
+        <label>Screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-2'>
+      <label>Widescreen screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-3'>
+        <label>Screen Type</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-4'>
+        <label> Touch Screen</label>
+        {/* <button>hgjhhf</button> */}
+      </div>
+
+
+
+
+      </div>
+
+
+
+
+
+<div className='Adminproductfeature-r2-4lbl-cover'>
+        
+        
+
+        <div className='Adminproductfeature-r2-col-1'>
+        <label>Screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-2'>
+      <label>Widescreen screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-3'>
+        <label>Screen Type</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-4'>
+        <label> Touch Screen</label>
+      </div>
+
+
+
+
+      </div>
+
+
+
+
+
+      <div className='Adminproductfeature-r2-4lbl-cover'>
+        
+        
+
+        <div className='Adminproductfeature-r2-col-1'>
+        <label>Screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-2'>
+      <label>Widescreen screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-3'>
+        <label>Screen Type</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-4'>
+        <label> Touch Screen</label>
+      </div>
+
+
+
+
+      </div>
+
+
+
+
+
+      <div className='Adminproductfeature-r2-4lbl-cover'>
+        
+        
+
+        <div className='Adminproductfeature-r2-col-1'>
+        <label>Screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-2'>
+      <label>Widescreen screen</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-3'>
+        <label>Screen Type</label>
+      </div>
+
+      <div className='Adminproductfeature-r2-col-4'>
+        <label> Add feature</label>
+      </div>
+
+
+
+
+      </div>
+
+
+
+
+
+<div className=' adminproductfeature-last-row'>
+  <label>Add Group feature</label>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Appss;
--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -6,7 +6,7 @@ import Offerlist from "./Offerlist";
 import TrendingProducts from "./TrendingProducts";
 import Orderlist from "./Orderlist";
 import Adminproductcostsummary from "./Adminproductcostsummary";
-
+import Appss from "./Appss";
 // import your route components too
 
 export default function Navigation() {
@@ -21,9 +21,15 @@ export default function Navigation() {
           <Route path="/Gridview" element={<Gridview />}></Route>
           <Route path="/Orderlist" element={<Orderlist />}></Route>
 
-          <Route path="/Offerlist" element={<Offerlist />}></Route>
-
           <Route path="/Adminproductcostsummary" element={<Adminproductcostsummary />}></Route>
+
+
+          <Route path="/Offerlist" element={<Offerlist />}></Route>  
+
+          
+          <Route path="/Appss" element={<Appss />}></Route>  
+
+          {/* <Route path="/Adminproductfeature" element={<Adminproductfeature />}></Route> */}
 
         </Routes>
       </HashRouter>
